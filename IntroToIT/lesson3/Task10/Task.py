c1122eb4daab6aaa105b88cdb602b39b7e558074
LiisaA228 <147524485+LiisaA228@users.noreply.github.com>
2023-10-17 20:03:31 +0300

    fixed

--- IntroToIT/lesson3/Task10/Task.py
+++ IntroToIT/lesson3/Task10/Task.py
@@ -1,6 +1,8 @@
 #INTRO TO IT 2nd COURSE
 s = input("Введите строку: ").replace(' ', '').lower()
 if s == s[::-1]:
+    s = int(input("Введите строку: ").replace(' ', '').lower())
+if s == s[::-1]:
     print("Строка является палиндромом")
 else:
     print("Строка не является палиндромом")
