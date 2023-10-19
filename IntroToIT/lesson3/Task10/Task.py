d8ed89ca673252436d9d9082c15c5fd749fb0abc
ElenInka <anonymousinkelen@gmail.com>
2023-10-20 00:44:39 +0400

    fix: variable s in the condition

--- IntroToIT/lesson3/Task10/Task.py
+++ IntroToIT/lesson3/Task10/Task.py
@@ -1,6 +1,6 @@
 #INTRO TO IT 2nd COURSE
 s = str(input("Введите строку: ").replace(' ', '').lower())
-if s = s[::-1]:
+if s[::-1]: # s = s[::-1] - опечатка
     print("Строка является палиндромом")
 else:
     print("Строка не является палиндромом")
