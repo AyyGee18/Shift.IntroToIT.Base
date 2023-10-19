0913c275d7598a00ff96eee9b5f5b11952d1d0ef
ElenInka <anonymousinkelen@gmail.com>
2023-10-20 00:57:07 +0400

    fix: == -> +=; refactor: sum -> summ;style: пробел

--- IntroToIT/lesson3/Task9/Task.py
+++ IntroToIT/lesson3/Task9/Task.py
@@ -1,6 +1,7 @@
 #INTRO TO IT 2nd COURSE
 n = int(input("Введите n: "))
-sum = 0
+summ = 0
 for i in range(1, n+1):
-    sum  == i
-print("Сумма чисел от 1 до", n, "равна:", sum)
+    summ  += i
+print("Сумма чисел от 1 до ", n, " равна: ", summ)# +=, пробелы, summ вместо sum,
+# чтобы не ругались другие текстовые редакторы для работы с Питоном
