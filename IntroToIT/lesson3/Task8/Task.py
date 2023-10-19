e281bf2260c929fc36239d413d8dc6fb59f0ef0d
EvaKoz <eviguzatova@mail.ru>
2023-10-19 17:37:58 +0300

    fix(lesson_3): all bugs fixed

--- IntroToIT/lesson3/Task8/Task.py
+++ IntroToIT/lesson3/Task8/Task.py
@@ -1,10 +1,11 @@
-#INTRO TO IT 2nd COURSE
+# INTRO TO IT 2nd COURSE
 
 def factorial(n):
-    if n = 0:
+    if n == 0:
         return 1
     else:
-        return n * factorial(n-1)
+        return n * factorial(n - 1)
+
 
 number = int(input("Введите число: "))
-print("Факториал числа:", factorial(n))
+print("Факториал числа:", factorial(number))
