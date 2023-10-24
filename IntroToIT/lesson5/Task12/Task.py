7803e6961e01953802d5e5b27af5bfb3617d5580
Vladislaw <vlad.kalinin044@gmail.com>
2023-10-24 20:16:11 +0300

    fix: Исправление ошибок решений

--- IntroToIT/lesson5/Task12/Task.py
+++ IntroToIT/lesson5/Task12/Task.py
@@ -1,11 +1,13 @@
 #INTRO TO IT 2nd COURSE
-# Задача 12: Вернуть список без дубликатов. 
+# Задача 12: Вернуть список без дубликатов.
 # Неправильное решение:
 def wrong_unique_elements(lst):
     new_lst = []
+    stop_lst = []
     for item in lst:
         if item in new_lst:
             new_lst.remove(item)
-        else:
+            stop_lst.append(item)
+        elif item not in stop_lst:
             new_lst.append(item)
     return new_lst
--- IntroToIT/lesson5/Task14/Task.py
+++ IntroToIT/lesson5/Task14/Task.py
@@ -1,9 +1,11 @@
 #INTRO TO IT 2nd COURSE
 
-# Задача 14: Сумма двух наибольших элементов списка. 
+# Задача 14: Сумма двух наибольших элементов списка.
 # Неправильное решение:
 def wrong_sum_of_two_largest(lst):
-    first_max = max(lst)
-    lst.remove(first_max)
-    second_max = max(lst)
+    lst_temp = lst.copy()
+    first_max = max(lst_temp)
+    lst_temp.remove(first_max)
+    second_max = max(lst_temp)
     return first_max + second_max
+
