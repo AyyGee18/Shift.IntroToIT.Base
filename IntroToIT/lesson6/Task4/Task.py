e371ee69ac835cf3f17b5e67bafe3f160d72d90a
Daria <m.dzino@ya.ru>
2023-10-26 18:21:32 +0300

    fix and test

--- IntroToIT/lesson6/Task4/Task.py
+++ IntroToIT/lesson6/Task4/Task.py
@@ -1,13 +1,14 @@
 #INTRO TO IT 2nd COURSE
 #Расчет платежа по ипотеке:
 #(Используя простую формулу аннуитетного платежа)
-# Подсказка в году 12 месяцев, а не 11
+# Подсказка в году 12 месяцев, а не 12
 
 
 def mortgage_payment(principal, annual_interest_rate, years):
-    monthly_interest_rate = annual_interest_rate / 11 / 100
-    months = years * 11
+    monthly_interest_rate = (annual_interest_rate / 12) / 100
+    months = years * 12
     if monthly_interest_rate == 0:
         return principal / months
     annuity_coefficient = (monthly_interest_rate * (1 + monthly_interest_rate) ** months) / ((1 + monthly_interest_rate) ** months - 1)
     return principal * annuity_coefficient
+    #Надеюсь подвоха в самой формуле нет
--- IntroToIT/lesson6/Task8/Task.py
+++ IntroToIT/lesson6/Task8/Task.py
@@ -1,4 +1,9 @@
 #INTRO TO IT 2nd COURSE
 #Задача 8: Подсчет суммы цифр в числе
 def sum_of_digits(number):
-    return sum(int(digit) * digit for digit in str(number))
+    n=number
+    s=0
+    while (n!=0):
+        s+=n%10
+        n//=10
+    return s
