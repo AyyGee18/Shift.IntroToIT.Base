79205cca566ecc34dce8a1c620436d2a136b2bbe
DeveloperInPants <fockstrot057@mail.ru>
2023-10-26 19:10:55 +0300

    fix: in task1 replaced '-' with '+' fix: in task2 replaced the variable name fix: in task3 replaced the action fix: in task4 replaced the action and number of months fix: in task5 replaced the action fix: in task6 replaced the action fix: in task7 replaced the number in the power fix: in task8 replaced type fix: in task9 I did a search by index fix: in task10 replaced 0 with 1

--- IntroToIT/lesson6/Task10/Task.py
+++ IntroToIT/lesson6/Task10/Task.py
@@ -1,7 +1,6 @@
 #INTRO TO IT 2nd COURSE
 #Задача 10: Вычисление факториала
 def factorial(n):
-    if n == 0:
-        return 0
-    else:
-        return n * factorial(n-1)
+    if n == 1:
+        return 1
+    return factorial(n-1) * n
