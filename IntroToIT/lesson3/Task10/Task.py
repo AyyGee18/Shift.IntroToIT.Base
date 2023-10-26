fd805a368786bff1c2c814e65e0da85960991c93
Maxon <maxn2703@gmail.com>
2023-10-26 19:18:06 +0300

    Max_Naz/4

--- IntroToIT/lesson3/Task10/Task.py
+++ IntroToIT/lesson3/Task10/Task.py
@@ -1,6 +1,8 @@
 #INTRO TO IT 2nd COURSE
-s = str(input("Введите строку: ").replace(' ', '').lower())
-if s = s[::-1]:
+s = str(input("Введите строку: ").replace(' ','').lower())
+if s == s[::-1]:
     print("Строка является палиндромом")
 else:
     print("Строка не является палиндромом")
+
+#str(input("Введите строку: "))
--- IntroToIT/lesson3/Task3/Task.py
+++ IntroToIT/lesson3/Task3/Task.py
@@ -1,5 +1,4 @@
 #INTRO TO IT 2nd COURSE
 a = 5
 b = 10
-a, b == a, b
-print(a, b)
+print(b, a)
