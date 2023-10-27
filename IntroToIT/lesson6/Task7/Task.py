2d00ccb49e7329df461583474fd18a1a7f9758d8
Nazelko <nazelko228@gmail.com>
2023-10-27 20:29:49 +0300

    fix: fixed bugs in the code

--- IntroToIT/lesson6/Task7/Task.py
+++ IntroToIT/lesson6/Task7/Task.py
@@ -1,4 +1,5 @@
 #INTRO TO IT 2nd COURSE
+from math import sqrt
 def heron_triangle_area(a, b, c):
     s = (a + b + c) / 2
-    return (s*(s-a)*(s-b)*(s-c)) * 0.5
+    return round(sqrt(s*(s-a)*(s-b)*(s-c)))
