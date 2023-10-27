43ee316ba35f1e40b0075ec14b38b10a4147e399
Satlykovs <satlykovs@gmail.com>
2023-10-27 18:24:50 +0300

    fixed: fixed all bugs in lesson 6 tasks 1 - 10; tests passed

--- IntroToIT/lesson5/Task1/Task.py
+++ IntroToIT/lesson5/Task1/Task.py
@@ -1,1 +1,12 @@
 #INTRO TO IT 2nd COURSE
+from itertools import product
+s = 'ЕКМОПРТЬ'
+combs = product(s, repeat=5)
+c = 0
+for comb in combs:
+    c += 1
+    st = ''.join(comb)
+    print(c, st)
+    if st[0] != 'Е' and st.count('К') == 2 and c % 2 == 0:
+        print(c, st)
+        break
