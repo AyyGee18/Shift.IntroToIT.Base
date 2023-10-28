30aeeb672b0b0c40d667dcaac633f68816df948f
game <_@example.com>
2023-10-28 10:04:20 -0700

    fix:("bugs fixed in lesson 6")

--- IntroToIT/lesson6/Task2/Task.py
+++ IntroToIT/lesson6/Task2/Task.py
@@ -2,10 +2,16 @@
 #Рассчитывание дня рождения:
 #(Пусть это будет функция, которая возвращает количество дней до дня рождения)
 from datetime import datetime
-
-def days_until_birthday(birZday):
+def days_until_birthday(birthday):
     today = datetime.today()
-    next_birthday = datetime(today.year, birZday.month, birZday.day)
+    next_birthday = datetime(today.year, birthday.month, birthday.day)
     if today > next_birthday:
-        next_birthday = datetime(today.year + 1, birZday.month, birZday.day)
-    return (next_birthday - today).days
+        next_birthday = datetime(today.year + 1, birthday.month, birthday.day)
+    days_until = (next_birthday - today).days
+    return days_until
+
+today = datetime(2023, 10, 27)
+birthday = datetime(2024, 12, 31)
+
+days = (birthday - today).days
+print(f"До вашего дня рождения осталось {days} дней.")
