07cef31aa4cfa69cf93f5b1cbe073e1759c9d792
game <_@example.com>
2023-10-28 09:51:02 -0700

    fix:("bugs fixed in lesson 6")

--- IntroToIT/lesson6/Task5/Task.py
+++ IntroToIT/lesson6/Task5/Task.py
@@ -3,4 +3,5 @@
 #(Предположим, что это простой кредит без процентов)
 
 def iphone_credit_cost(iphone_price, months):
-    return iphone_price ** months
+    monthly_payment = iphone_price / months
+    return monthly_payment
