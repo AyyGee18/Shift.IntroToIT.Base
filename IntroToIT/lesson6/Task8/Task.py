ec568b7cb007f2092e0d5fe6f9bbeccd241511d0
Maxon <maxn2703@gmail.com>
2023-10-28 20:10:31 +0300

    It's OKey

--- IntroToIT/lesson6/Task8/Task.py
+++ IntroToIT/lesson6/Task8/Task.py
@@ -1,4 +1,7 @@
 #INTRO TO IT 2nd COURSE
 #Задача 8: Подсчет суммы цифр в числе
 def sum_of_digits(number):
-    return sum(int(digit) * digit for digit in str(number))
+    sum = 0
+    for digit in str(number):
+        sum += int(digit)
+    return sum
