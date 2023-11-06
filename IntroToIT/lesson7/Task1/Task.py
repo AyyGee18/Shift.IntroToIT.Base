5255d4f274766d6544caad077c2ab7a13211cbb0
venimu <ven1mu@yandex.ru>
2023-11-06 18:25:06 +0300

    refactor(lesson7) Task1: commented functions Task2: changed names and refactored code

--- IntroToIT/lesson7/Task1/Task.py
+++ IntroToIT/lesson7/Task1/Task.py
@@ -1,19 +1,20 @@
 #INTRO TO IT 2nd COURSE
+# sum of two numbers
 def add_numbers(a, b):
 
     result = a + b
     return result
-
+# multiplication of numbers
 def multiply_numbers(a, b):
 
     result = a * b
     return result
-
+# max in arr
 def find_max_number(numbers):
 
     max_number = max(numbers)
     return max_number
-
+# factorial of number
 def calculate_factorial(n):
  
     if n == 0:
@@ -22,7 +23,7 @@ def calculate_factorial(n):
     for i in range(1, n + 1):
         factorial *= i
     return factorial
-
+# check of eventity
 def is_even(number):
 
     if number % 2 == 0:
@@ -30,15 +31,20 @@ def is_even(number):
     else:
         return False
 
+# some nums
 num1 = 10
 num2 = 5
+#func calls
 sum_result = add_numbers(num1, num2)
 product_result = multiply_numbers(num1, num2)
+# some arr
 numbers_list = [3, 8, 1, 6, 12]
+#func calls
 max_num = find_max_number(numbers_list)
 factorial_result = calculate_factorial(5)
 is_even_num = is_even(7)
 
+# out
 print(f"Сумма чисел {num1} и {num2} равна {sum_result}")
 print(f"Произведение чисел {num1} и {num2} равно {product_result}")
 print(f"Наибольшее число в списке {numbers_list} - {max_num}")
