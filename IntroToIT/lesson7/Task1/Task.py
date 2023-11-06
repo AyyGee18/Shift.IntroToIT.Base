9427bde970468fbf4c28b22e3645482590bee73b
Nazelko <nazelko228@gmail.com>
2023-11-06 19:37:48 +0300

    refactor: code refactoring

--- IntroToIT/lesson7/Task1/Task.py
+++ IntroToIT/lesson7/Task1/Task.py
@@ -1,21 +1,42 @@
 #INTRO TO IT 2nd COURSE
 def add_numbers(a, b):
+    """
+    Функция для сложения двух чисел
+    :param a: Первое число
+    :param b: Второе число
+    :result: Сумма чисел
+    """
 
     result = a + b
     return result
 
 def multiply_numbers(a, b):
-
+    """
+    Функция для умножения двух чисел
+    :param a: Первое число
+    :param b: Второе число
+    :result: Умножение чисел
+    """
     result = a * b
     return result
 
 def find_max_number(numbers):
+    """
+    Функция для нахождения максималного числа
+    :numbers: список чисел
+    :max_number: Максимальное число
+    """
 
     max_number = max(numbers)
     return max_number
 
 def calculate_factorial(n):
- 
+    """
+    Функция для нахождения Факториала числа
+    :n: число, факториал которого мы ищем
+    :factorial: Факториал числа
+    """
+    #Предупреждение: этот код может вызвать ошибку, если x меньше 0
     if n == 0:
         return 1
     factorial = 1
@@ -24,14 +45,23 @@ def calculate_factorial(n):
     return factorial
 
 def is_even(number):
+    """
+    Функция для определения четности числа
+    :number: число, которое мы проверяем
+    :return: True - число четное, false - нечетное
+    """   
 
     if number % 2 == 0:
         return True
     else:
         return False
 
+
+#Задаем переменные num1 и num2
 num1 = 10
 num2 = 5
+
+#Работаем с функциями
 sum_result = add_numbers(num1, num2)
 product_result = multiply_numbers(num1, num2)
 numbers_list = [3, 8, 1, 6, 12]
@@ -39,6 +69,7 @@ max_num = find_max_number(numbers_list)
 factorial_result = calculate_factorial(5)
 is_even_num = is_even(7)
 
+#Печатаем результаты
 print(f"Сумма чисел {num1} и {num2} равна {sum_result}")
 print(f"Произведение чисел {num1} и {num2} равно {product_result}")
 print(f"Наибольшее число в списке {numbers_list} - {max_num}")
