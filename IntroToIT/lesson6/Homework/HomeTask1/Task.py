c06afd037c147c29df386809bb65120ee80166e3
EvaKoz <eviguzatova@mail.ru>
2023-11-06 18:18:40 +0300

    fix(lesson6): all buds fixed again

--- IntroToIT/lesson6/Homework/HomeTask1/Task.py
+++ IntroToIT/lesson6/Homework/HomeTask1/Task.py
@@ -1,5 +1,5 @@
-#INTRO TO IT 2nd COURSE
-#Давайте начнем с написания функций:
+# INTRO TO IT 2nd COURSE
+# Давайте начнем с написания функций:
 # Исправьте код так, чтобы он выводил "Hello, World!"
 
-print("Hello World!)
+print("Hello World!")
--- IntroToIT/lesson6/Homework/HomeTask3/Task.py
+++ IntroToIT/lesson6/Homework/HomeTask3/Task.py
@@ -4,7 +4,8 @@
 x = 10
 y = 20
 
+t = x
 x = y
-y = x
+y = t
 
 print(x, y)
