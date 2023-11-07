2296358c59d3358637807280a0d58d65096f17ad
Vladislaw <vlad.kalinin044@gmail.com>
2023-11-07 20:05:04 +0300

    docs: Добавлен кодстайл в Task 1 fix: Исправлены ошибки связанные с уровнями кода

--- IntroToIT/lesson7/Task1/Task.py
+++ IntroToIT/lesson7/Task1/Task.py
@@ -1,21 +1,33 @@
 #INTRO TO IT 2nd COURSE
 def add_numbers(a, b):
-
+    '''Функция сложения числе
+    param a: первое число
+    param b: Второе число
+    '''
     result = a + b
     return result
 
 def multiply_numbers(a, b):
-
+    '''Функция умножение чисел
+    param a: 1 число
+    param b: 2 число
+    '''
     result = a * b
     return result
 
 def find_max_number(numbers):
-
+    '''Функция обнаружения наибольшего числа из списка
+    param number: список
+    param max_number: наибольшее число
+    '''
     max_number = max(numbers)
     return max_number
 
 def calculate_factorial(n):
- 
+    '''Функция факториала
+    param n: вводимое число
+    param factorial: результат факториала
+    '''
     if n == 0:
         return 1
     factorial = 1
@@ -24,7 +36,9 @@ def calculate_factorial(n):
     return factorial
 
 def is_even(number):
-
+    '''Функция проверки на чётность
+    param number: вводимое число
+    '''
     if number % 2 == 0:
         return True
     else:
@@ -32,13 +46,18 @@ def is_even(number):
 
 num1 = 10
 num2 = 5
+#Сложение чисел
 sum_result = add_numbers(num1, num2)
+#Умножение чисел
 product_result = multiply_numbers(num1, num2)
 numbers_list = [3, 8, 1, 6, 12]
+#Поиск максимального значения
 max_num = find_max_number(numbers_list)
+#Факториал
 factorial_result = calculate_factorial(5)
+#Проверка на чётность
 is_even_num = is_even(7)
-
+#Выводы результатов
 print(f"Сумма чисел {num1} и {num2} равна {sum_result}")
 print(f"Произведение чисел {num1} и {num2} равно {product_result}")
 print(f"Наибольшее число в списке {numbers_list} - {max_num}")
