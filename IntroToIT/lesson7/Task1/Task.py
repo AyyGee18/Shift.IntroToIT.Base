c353a6796f10d41524d35dbaa9aacad90ac3b7b4
EvaKoz <eviguzatova@mail.ru>
2023-11-10 17:40:07 +0300

    refactor(lesson7): Code in folders Task1 and Task2

--- IntroToIT/lesson7/Task1/Task.py
+++ IntroToIT/lesson7/Task1/Task.py
@@ -1,21 +1,24 @@
-#INTRO TO IT 2nd COURSE
+# INTRO TO IT 2nd COURSE
+# сумма двух переменных
 def add_numbers(a, b):
-
     result = a + b
     return result
 
-def multiply_numbers(a, b):
 
+# произведение двух переменных
+def multiply_numbers(a, b):
     result = a * b
     return result
 
-def find_max_number(numbers):
 
+# нахождение максимального числа
+def find_max_number(numbers):
     max_number = max(numbers)
     return max_number
 
+
+# вычисление факториала числа n
 def calculate_factorial(n):
- 
     if n == 0:
         return 1
     factorial = 1
@@ -23,15 +26,20 @@ def calculate_factorial(n):
         factorial *= i
     return factorial
 
-def is_even(number):
 
+# проверка числа на чётность
+def is_even(number):
     if number % 2 == 0:
         return True
     else:
         return False
 
+
+# инициализация переменных
 num1 = 10
 num2 = 5
+
+# вызов функций
 sum_result = add_numbers(num1, num2)
 product_result = multiply_numbers(num1, num2)
 numbers_list = [3, 8, 1, 6, 12]
@@ -39,6 +47,7 @@ max_num = find_max_number(numbers_list)
 factorial_result = calculate_factorial(5)
 is_even_num = is_even(7)
 
+# вывод в консоль
 print(f"Сумма чисел {num1} и {num2} равна {sum_result}")
 print(f"Произведение чисел {num1} и {num2} равно {product_result}")
 print(f"Наибольшее число в списке {numbers_list} - {max_num}")
