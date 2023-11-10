58fae41a848c3a00ae22fc2c347b47e1a3b79aeb
Nazelko <nazelko228@gmail.com>
2023-11-10 21:29:20 +0300

    fix: code fixed
    docs: some codes documented

--- IntroToIT/lesson8/Task4/Task.py
+++ IntroToIT/lesson8/Task4/Task.py
@@ -1,9 +1,10 @@
 #INTRO TO IT 2nd COURSE
 # Задача: проверить, является ли число простым
+from math import sqrt
 def is_prime(num):
     if num <= 1:
         return False
-    for i in range(2, num):  # верхняя граница в цикле не правильная
+    for i in range(2, int(sqrt(num))+1):  # верхняя граница в цикле не правильная
         if num % i == 0:
             return False
     return True
