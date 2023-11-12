0c089f55a2a068c89318c5fe8f39d39d79712d2d
Vanya <ivantimofeev0006@gmail.com>
2023-11-12 17:38:17 +0300

    11.11.23

--- IntroToIT/lesson8/Task1/Task.py
+++ IntroToIT/lesson8/Task1/Task.py
@@ -1,7 +1,13 @@
 #INTRO TO IT 2nd COURSE
 # Задача: вычислить сумму чисел в списке
 def sum_list(lst):
+    """
+    Функция вычесления суммы чисел списка.
+    :lst: Список чисел.
+    :return: Сумма чисел.
+    """
     total = 0
     for num in lst:
-        pass  # здесь должно быть сложение
+        total += num
+        pass # здесь должно быть сложение
     return total
