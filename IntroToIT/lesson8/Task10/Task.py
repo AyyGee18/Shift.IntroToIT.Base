12835b89fa1d197c77f64f5cbac8916dc5473045
ElenInka <anonymousinkelen@gmail.com>
2023-11-16 17:41:29 +0400

    fix: some bugs fixed; style: Task 10

--- IntroToIT/lesson8/Task10/Task.py
+++ IntroToIT/lesson8/Task10/Task.py
@@ -1,9 +1,11 @@
 #INTRO TO IT 2nd COURSE
 # Задача: найти второе наибольшее число в списке
 def second_largest(numbers):
-    first = second = float('-inf')
-    for n in numbers:
-        first = n  # Ошибка в логике определения первого и второго наибольшего
+    first = [max(numbers)]
+    second = float('-inf')
+    while any(element in first for element in numbers):
+        numbers.remove(max(numbers))
+    second = max(numbers)
     return second if second != float('-inf') else None
 
 print(second_largest([10, 4, 9, 4, 9, 10, 4]))  # Должно вывести 9
